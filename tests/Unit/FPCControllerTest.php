47bc7975cb232d5dba0b428195f23df9b728648a
Pablo C <pablo.cogua@tai-software.com>
2024-03-20 16:09:28 -0500

    second

---
+++ tests/Unit/FPCControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Controllers\FPCController;
+use Illuminate\Http\JsonResponse;
+
+class FPCControllerTest extends TestCase
+{
+    public function test_get_firstteam()
+    {
+        //Arrange
+        $controller = new FPCController();
+
+        // Act
+        $response = $controller->FirsTeam();
+        $data = $response->getData(true);
+
+        // Assert
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertTrue($data['Points'] <= 30);
+    }
+}
